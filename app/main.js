02b57cd164251ae037ab4af1831174539bba2772
cy <c1uan@outlook.com>
2025-09-27 20:44:13 +0800

    dependency: 添加uview-plus

--- app/main.js
+++ app/main.js
@@ -2,9 +2,12 @@ import App from './App'
 import { createSSRApp } from 'vue';
 import * as Pinia from 'pinia';
 
+import uviewPlus from '@/uni_modules/uview-plus'
+
 export function createApp() {
 	const app = createSSRApp(App);
 	app.use(Pinia.createPinia());
+	app.use(uviewPlus)
 	return {
 		app,
 		Pinia, // 此处必须将 Pinia 返回
