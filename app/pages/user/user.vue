97efa0d5675c9f3cf985bafddffdfc70aa578b81
cy <c1uan@outlook.com>
2025-10-23 00:12:15 +0800

    rename: my-orders.vue

--- app/pages/user/user.vue
+++ app/pages/user/user.vue
@@ -51,7 +51,7 @@
 
 	// 功能列表
 	const functionList = ref([
-		{ id: 1, name: '我的订单', icon: 'list', path: '/pages/user/orders/myOrders' },
+		{ id: 1, name: '我的订单', icon: 'list', path: '/pages/user/orders/my-orders' },
 		{ id: 2, name: '我的钱包', icon: 'wallet', path: '/pages/wallet/index' },
 		{ id: 3, name: '我的地址', icon: 'location', path: '/pages/user/address/address' },
 		{ id: 4, name: '设置', icon: 'gear', path: '/pages/settings/index' },
