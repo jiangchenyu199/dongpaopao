109542ab94f01fa025dc9a97b5699a558b817949
cy <c1uan@outlook.com>
2026-01-06 00:38:19 +0800

    feat: set ElementPlus locale to Chinese

--- admin/src/main.js
+++ admin/src/main.js
@@ -6,7 +6,10 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 import naive from 'naive-ui'
+
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
@@ -14,6 +17,8 @@ pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+    locale: zhCn
+})
 app.use(naive)
 app.mount('#app')
