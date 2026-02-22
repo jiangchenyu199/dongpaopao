6622f85a11828574a858fcd713b61d1553d1d2dc
cy <c1uan@outlook.com>
2026-02-22 22:25:50 +0800

    feat(admin): enable HTTPS support in Vite configuration

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 import { resolve } from 'path'
+import fs from 'node:fs'
+
+const certDir = resolve(__dirname, '../certs')
+const httpsOpt = fs.existsSync(resolve(certDir, 'cert.pem'))
+  ? {
+    key: fs.readFileSync(resolve(certDir, 'key.pem')),
+    cert: fs.readFileSync(resolve(certDir, 'cert.pem'))
+  }
+  : undefined
 
 export default defineConfig({
   plugins: [vue()],
@@ -10,6 +19,9 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src')
     }
+  },
+  server: {
+    https: httpsOpt
   },
   css: {
     postcss: {
